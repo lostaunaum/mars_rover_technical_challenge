89649a716397a42a4d2fb22c9573441af19af846
Marco Lostaunau <MLostaunau@newgistics.com>
2018-08-31 10:36:56 -0500

    First Push.

---
+++ test/FunctionalTests/RoverControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FunctionalTests
+{
+    public class RoverControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
